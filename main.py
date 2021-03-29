21471380991a3cbe703ed58835dfeaa7f622125f
20160125 <ezn1020@kumoh.ac.kr>
2021-03-29 10:13:55 +0900

    * Add: Main - 메인 함수 작성

---
+++ main.py
@@ -0,0 +1,46 @@
+import argparse
+from tensorflow.python.keras.engine.training import Model
+import yaml
+
+from data_loader import DataLoader
+from data_generator import DataGenerator
+from graph_handler import GraphHandler
+from models.model_on_graph import ModelOnGraph
+from agent import Agent
+
+
+def compute_config(config):
+    keys = config['data_params']['key']
+    config['model_params']['p'] = len(keys)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, required=True)
+    parser.add_argument('--test_only', action='store_true')
+    args = parser.parse_args()
+
+    with open(args.config) as f:
+        config = yaml.load(f)
+    compute_config(config)
+
+    print('[Task] Load Data Loader')
+    data_loader = DataLoader(config)
+    print('[Done] Loaded Data Loader')
+    print('[Task] Load Data Generator')
+    data_gen = DataGenerator(config, data_loader)
+    print('[Done] Loaded Data Generator')
+    print('[Task] Load Graph Handler')
+    graph_handler = GraphHandler(config, data_gen)
+    print('[Done] Loaded Graph Handler')
+    print('[Task] Build Model')
+    model_on_graph = ModelOnGraph(config)
+    print('[Done] Built Model')
+    print('[Task] Load Agent')
+    agent = Agent(config, graph_handler, model_on_graph)
+    print('[Done] Loaded Agent')
+
+    if args.test_only:
+        agent.run_test()
+    else:
+        agent.running()
